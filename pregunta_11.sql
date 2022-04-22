8ec9d49a7cd3066328afcdcbd7c8368143ed0304
pbedoyao <101910326+pbedoyao@users.noreply.github.com>
2022-04-22 17:05:28 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,4 +38,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT COUNT(*) FROM tbl1 GROUP BY SUBSTRING(c14, 1, 4) HAVING SUBSTRING(c14, 1, 4)='2018';
+SELECT COUNT(*) FROM tbl1 GROUP BY SUBSTR(c14, 1, 4) HAVING SUBSTR(c14, 1, 4)='2018';
